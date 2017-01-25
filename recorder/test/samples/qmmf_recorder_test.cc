ceaf75a433fdb84d309ffcf24f2b5a2d1b74bb06
Partha Mitra <pmitra@codeaurora.org>
2017-01-25 16:18:25 +0530

    recorder_test: Add missing breaks
    Add missing breaks in Command Menu Switch Cases
    
    Change-Id: Ifbc8808ad8bb72cdfaca4afd6b0c3ce50226a329

--- recorder/test/samples/qmmf_recorder_test.cc
+++ recorder/test/samples/qmmf_recorder_test.cc
@@ -3780,6 +3780,7 @@ int main(int argc,char *argv[]) {
       case CmdMenu::CREATE_PCM_G7ll_AUD_SESSION_CMD: {
           test_context.CreateAudioPCMG711Track();
       }
+      break;
       case CmdMenu::CREATE_RDI_SESSION_CMD: {
           test_context.SessionRDITrack();
       }
@@ -3839,6 +3840,7 @@ int main(int argc,char *argv[]) {
       case CmdMenu::IR_MODE_CMD: {
         test_context.ToggleIR();
       }
+      break;
       case CmdMenu::CHOOSE_CAMERA_CMD: {
         test_context.ChooseCamera();
       }
