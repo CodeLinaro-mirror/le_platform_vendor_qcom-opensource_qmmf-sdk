5ac47b68f4ddc3d753aeced41096a4c65f317989
Petar Georgiev <petarg@codeaurora.org>
2017-02-02 18:05:41 +0200

    recorder_gtest: Implemented separate test for 360 Camera
    List of testcases:
      - Create & Delete Session Api with a MultiCamera instance.
      - MultiCamera session with one 3840x1920 YUV track at 30 fps.
      - MultiCamera JPEG image capture with resolution 6080x3040.
      - MultiCamera session with one 3840x1920 h264 track at 30 fps
    
    Change-Id: I2bba80ffb8c48edfe70731be3e4f89e823814699

--- recorder/test/gtest/Android.mk
+++ recorder/test/gtest/Android.mk
@@ -21,4 +21,19 @@ LOCAL_MODULE = qmmf_recorder_gtest
 
 include $(BUILD_NATIVE_TEST)
 
+# Build recorder 360 camera test application binary
+
+include $(CLEAR_VARS)
+
+include $(QMMF_SDK_TOP_SRCDIR)/common.mk
+
+LOCAL_SRC_FILES := qmmf_recorder_360cam_gtest.cc
+
+LOCAL_SHARED_LIBRARIES += libqmmf_recorder_client libqmmf_av_queue
+LOCAL_SHARED_LIBRARIES += libcamera_client
+
+LOCAL_MODULE = qmmf_recorder_360cam_gtest
+
+include $(BUILD_NATIVE_TEST)
+
 endif # BUILD_QMMMF
