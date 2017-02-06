d1a84da41fa7b49c0d20933f55f6dcd95be7bd67
Evgeniy Borisov <gencho@codeaurora.org>
2017-02-06 16:17:10 +0200

    recorder: Port PostProcess class implementation
    Change-Id: I11c8ac6f66a81817cc51c5f174c97e7769fd64cb

--- recorder/src/service/Android.mk
+++ recorder/src/service/Android.mk
@@ -20,6 +20,11 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmmf-alg/
 LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/media
 LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display
 
+# reprocess-related includes
+LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/camera/QCamera2/stack/common \
+LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/camera/mm-image-codec/qomx_core \
+LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/camera/mm-image-codec/qexif \
+
 LOCAL_SRC_FILES := qmmf_recorder_service.cc
 LOCAL_SRC_FILES += qmmf_recorder_impl.cc
 LOCAL_SRC_FILES += qmmf_recorder_ion.cc
@@ -32,6 +37,8 @@ LOCAL_SRC_FILES += qmmf_audio_raw_track_source.cc
 LOCAL_SRC_FILES += qmmf_audio_encoded_track_source.cc
 LOCAL_SRC_FILES += qmmf_audio_encoder_core.cc
 LOCAL_SRC_FILES += qmmf_multicamera_manager.cc
+LOCAL_SRC_FILES += qmmf_jpeg_encoder.cc
+LOCAL_SRC_FILES += qmmf_camera_jpeg.cc
 
 LOCAL_SHARED_LIBRARIES += libqmmf_recorder_client libqmmf_camera_adaptor
 LOCAL_SHARED_LIBRARIES += libqmmf_codec_adaptor libqmmf_audio_client
