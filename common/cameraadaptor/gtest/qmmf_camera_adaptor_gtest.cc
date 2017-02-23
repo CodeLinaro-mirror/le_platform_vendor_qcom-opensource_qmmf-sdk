e52cc3e9a3539065b485d49500399843f2055323
Iliya Varadzhakov <ivarad@codeaurora.org>
2017-02-23 15:14:40 +0200

    cameraadaptor: Add test case for exposure metering mode
    Auto Exposure metering mode needs to be verified. The test
    reads the capabilities fo the camera and use the supported
    modes to produce output yuv frame for every mode.
    
    Change-Id: Id4197528004a29770377ac36fb29500b4fdf1be5

--- common/cameraadaptor/gtest/qmmf_camera_adaptor_gtest.cc
+++ common/cameraadaptor/gtest/qmmf_camera_adaptor_gtest.cc
@@ -1005,6 +1005,86 @@ TEST_F(Camera3Gtest, Video1080pExposureModes) {
   }
 }
 
+TEST_F(Camera3Gtest, Video1080pExposureMeteringModes) {
+  CameraStreamParameters streamParams;
+  Camera3Request videoRequest;
+  int64_t lastFrameNumber;
+  int32_t repeatingStreamId, videoRequestId;
+  CameraMetadata staticInfo;
+
+  auto ret = device_client_->GetCameraInfo(camera_idx_, &staticInfo);
+  ASSERT_EQ(0, ret);
+
+  if (staticInfo.exists(QCAMERA3_EXPOSURE_METER_AVAILABLE_MODES)) {
+    camera_metadata_entry metaEntry =
+        staticInfo.find(QCAMERA3_EXPOSURE_METER_AVAILABLE_MODES);
+
+    ret = device_client_->BeginConfigure();
+    ASSERT_EQ(0, ret);
+
+    memset(&streamParams, 0, sizeof(streamParams));
+    streamParams.bufferCount = STREAM_BUFFER_COUNT;
+    streamParams.format = HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED;
+    streamParams.width = 1920;
+    streamParams.height = 1080;
+    streamParams.grallocFlags =
+      GRALLOC_USAGE_HW_FB | private_handle_t::PRIV_FLAGS_VIDEO_ENCODER;
+    streamParams.cb = [&](int32_t streamId,
+              StreamBuffer buffer) { StreamCbDumpNVXX(streamId, buffer); };
+
+    // 1080p Stream1
+    repeatingStreamId = device_client_->CreateStream(streamParams);
+    ASSERT_GE(repeatingStreamId, 0);
+    videoRequest.streamIds.add(repeatingStreamId);
+
+    ret = device_client_->EndConfigure();
+    ASSERT_EQ(0, ret);
+
+    ret = device_client_->CreateDefaultRequest(CAMERA3_TEMPLATE_VIDEO_RECORD,
+                        &videoRequest.metadata);
+    ASSERT_EQ(0, ret);
+
+    ret = device_client_->SubmitRequest(videoRequest, true, &lastFrameNumber);
+    ASSERT_GE(ret, 0);
+    videoRequestId = ret;
+
+    while (metaEntry.count) {
+      --metaEntry.count;
+      printf("Exposure metering mode: %d\n", metaEntry.data.i32[metaEntry.count]);
+
+      ret = device_client_->CreateDefaultRequest(CAMERA3_TEMPLATE_VIDEO_RECORD,
+                          &videoRequest.metadata);
+      ASSERT_EQ(0, ret);
+
+      videoRequest.metadata.update(QCAMERA3_EXPOSURE_METER,
+                                    &metaEntry.data.i32[metaEntry.count],
+                                    1);
+
+      ret = device_client_->SubmitRequest(videoRequest, true, &lastFrameNumber);
+      ASSERT_GE(ret, 0);
+      videoRequestId = ret;
+
+      // Run video for some time
+      sleep(5);
+
+      dump_yuv_ = true;
+    }
+
+    ret = device_client_->CancelRequest(videoRequestId, &lastFrameNumber);
+    ASSERT_EQ(0, ret);
+
+    printf("%s: Video request cancelled last frame number: %" PRId64 "\n",
+       __func__, lastFrameNumber);
+
+    ret = device_client_->WaitUntilIdle();
+    ASSERT_EQ(0, ret);
+
+    ret = device_client_->DeleteStream(repeatingStreamId, true);
+    ASSERT_EQ(0, ret);
+    ASSERT_FALSE(camera_error_);
+  }
+}
+
 TEST_F(Camera3Gtest, Video1080pSnapshotHDR) {
   CameraStreamParameters streamParams;
   Camera3Request previewRequest, snapshotRequest;
