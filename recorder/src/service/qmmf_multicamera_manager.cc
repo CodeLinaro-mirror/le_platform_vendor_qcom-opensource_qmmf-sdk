dde34eccf669bfbe0cc5532a4194105459582c07
Ajit Singh Solanki <ajits@codeaurora.org>
2017-02-22 20:22:16 -0800

    recorder: update stitching wrapper lib name
    Change-Id: I64ff4af375f681ff07ec772620466a4e44a72eaf

--- recorder/src/service/qmmf_multicamera_manager.cc
+++ recorder/src/service/qmmf_multicamera_manager.cc
@@ -53,7 +53,7 @@ namespace recorder {
 
 
 #ifdef LE_BUILD
-static const char *kStitchLib = "/usr/lib/libEngine_QmmfAlgPolaris.so.0";
+static const char *kStitchLib = "/usr/lib/libqmmf_alg_polaris_stitch.so.0";
 #else
 static const char *kStitchLib = "/vendor/lib/libqmmf_alg_polaris_stitch.so";
 #endif
