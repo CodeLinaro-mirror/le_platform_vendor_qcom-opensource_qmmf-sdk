62a1774781db63b4eab6a6b71ca5b0e15942c0e4
Ajit Singh Solanki <ajits@codeaurora.org>
2017-07-24 20:12:53 -0700

    Recorder: Add support for RAW16.
    Add RAW16 format in ValidateInput & ValidateOutput
    format methods.
    
    Change-Id: I09b905354a6e8fb7bf089db2f15d47dc650915d0

--- recorder/src/service/post-process/modules/camera-hal-reproc/qmmf_camera_hal_reproc.cc
+++ recorder/src/service/post-process/modules/camera-hal-reproc/qmmf_camera_hal_reproc.cc
@@ -161,7 +161,7 @@ PostProcCreateParam CameraHalReproc::GetInput(const PostProcCreateParam &out) {
   // Save the output parameters as well, we will need them later.
   input_param_ = output_param_ = out;
   // work around since HAL does not report supported formats correctly
-  input_param_.format = HAL_PIXEL_FORMAT_RAW10;
+  input_param_.format = HAL_PIXEL_FORMAT_RAW16;
   return input_param_;
 }
 
@@ -192,7 +192,9 @@ status_t CameraHalReproc::ValidateInput(const PostProcCreateParam &input) {
     if ((entry.data.i32[i] == input.format &&
         HAL_PIXEL_FORMAT_RAW10 == input.format) ||
         (entry.data.i32[i] == input.format &&
-        HAL_PIXEL_FORMAT_RAW12 == input.format)) {
+        HAL_PIXEL_FORMAT_RAW12 == input.format) ||
+        (entry.data.i32[i] == input.format &&
+        HAL_PIXEL_FORMAT_RAW16 == input.format)) {
       supported = true;
       break;
     }
