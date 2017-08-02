421f16d01b3b32c999f892a418d834fe5364d253
Mohammed Thasleem <mthasl@codeaurora.org>
2017-08-02 19:10:25 +0530

    Revert "qmmf-sdk: gtest: add support stream to 4K snapshot test".
    This reverts commit 6f1ad04287d38d4663618c206c0dfad6bc470dcc.
    
    Change-Id: I9478d3ef7a61525326e87d0692f17d72670b8838

--- recorder/test/gtest/qmmf_recorder_gtest.cc
+++ recorder/test/gtest/qmmf_recorder_gtest.cc
@@ -1280,52 +1280,6 @@ TEST_F(RecorderGtest, 4KSnapshot) {
   ret = recorder_.StartCamera(camera_id_, camera_start_params_);
   assert(ret == NO_ERROR);
 
-  SessionCb session_status_cb;
-  session_status_cb.event_cb = [this] (EventType event_type, void *event_data,
-                                       size_t event_data_size) -> void {
-      SessionCallbackHandler(event_type, event_data, event_data_size); };
-
-  uint32_t session_id;
-  ret = recorder_.CreateSession(session_status_cb, &session_id);
-  assert(session_id > 0);
-  assert(ret == NO_ERROR);
-
-  TrackCb video_track_cb;
-  video_track_cb.event_cb =
-      [this] (uint32_t track_id, EventType event_type,
-              void *event_data, size_t event_data_size) -> void {
-      VideoTrackEventCb(track_id, event_type, event_data, event_data_size); };
-
-  uint32_t video_track_id = 1;
-  VideoTrackCreateParam video_track_param;
-  memset(&video_track_param, 0x0, sizeof video_track_param);
-
-  video_track_param.camera_id      = camera_id_;
-  video_track_param.width          = 640;
-  video_track_param.height         = 480;
-  video_track_param.frame_rate     = 30;
-  video_track_param.format_type    = VideoFormat::kAVC;
-  video_track_param.low_power_mode = false;
-
-  video_track_cb.data_cb = [&, session_id] (uint32_t track_id,
-      std::vector<BufferDescriptor> buffers,
-      std::vector<MetaData> meta_buffers) {
-        VideoTrackTwoEncDataCb(session_id, track_id, buffers, meta_buffers);
-      };
-
-  ret = recorder_.CreateVideoTrack(session_id, video_track_id,
-                                   video_track_param, video_track_cb);
-  assert(ret == NO_ERROR);
-
-  std::vector<uint32_t> track_ids = {video_track_id};
-  sessions_.insert(std::make_pair(session_id, track_ids));
-
-  ret = recorder_.StartSession(session_id);
-  assert(ret == NO_ERROR);
-
-  // Record for sometime
-  sleep(1);
-
   ImageParam image_param;
   memset(&image_param, 0x0, sizeof image_param);
   image_param.width         = 3840;
@@ -1359,35 +1313,23 @@ TEST_F(RecorderGtest, 4KSnapshot) {
   }
   assert (res_supported != false);
 
-  ImageCaptureCb cb = [this] (uint32_t camera_id, uint32_t image_count,
-                              BufferDescriptor buffer,
-                              MetaData meta_data) -> void
-      { SnapshotCb(camera_id, image_count, buffer, meta_data); };
-
-  for (uint32_t i = 1; i <= iteration_count_; i++) {
+  for(uint32_t i = 1; i <= iteration_count_; i++) {
     fprintf(stderr,"test iteration = %d/%d\n", i, iteration_count_);
     TEST_INFO("%s:%s: Running Test(%s) iteration = %d ", TAG, __func__,
         test_info_->name(), i);
+
+    ImageCaptureCb cb = [this] (uint32_t camera_id, uint32_t image_count,
+                                BufferDescriptor buffer,
+                                MetaData meta_data) -> void
+        { SnapshotCb(camera_id, image_count, buffer, meta_data); };
+
     meta_array.push_back(meta);
-    ret = recorder_.CaptureImage(camera_id_, image_param, 1, meta_array, cb);
+    ret = recorder_.CaptureImage(camera_id_, image_param, 1, meta_array,
+                                 cb);
     assert(ret == NO_ERROR);
     // Take snapshot after every 5 sec.
     sleep(5);
   }
-
-  ret = recorder_.CancelCaptureImage(camera_id_);
-  assert(ret == NO_ERROR);
-
-  ret = recorder_.StopSession(session_id, false);
-  assert(ret == NO_ERROR);
-
-  ret = recorder_.DeleteVideoTrack(session_id, video_track_id);
-  assert(ret == NO_ERROR);
-
-  ret = recorder_.DeleteSession(session_id);
-  assert(ret == NO_ERROR);
-
-  ClearSessions();
 
   ret = recorder_.StopCamera(camera_id_);
   assert(ret == NO_ERROR);
@@ -1397,6 +1339,7 @@ TEST_F(RecorderGtest, 4KSnapshot) {
 
   fprintf(stderr,"---------- Test Completed %s.%s ----------\n",
       test_info_->test_case_name(), test_info_->name());
+
 }
 
 /*
