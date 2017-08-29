8d2d90756e39ab384888656b63fcadb529d3d667
Petar G. Georgiev <petarg@codeaurora.org>
2017-08-29 18:02:26 +0300

    recorder: MultiCamera: Properly Start/Stop active streams
    Issue: Track ids were incorrectly added to active_streams_
      vector at the end of CreateStream() API. This causes streams
      that were not actually running before a new call to the
      CreateStream() API, to be initiated.
    
    Fix: Track ids are added/removed from active_streams_ vector
      on Start/StopStream APIs.
    
    Change-Id: I6ef28d4904de1b4ae3d131a85a1c10138f4975f4

--- recorder/src/service/qmmf_multicamera_manager.h
+++ recorder/src/service/qmmf_multicamera_manager.h
@@ -167,7 +167,7 @@ class MultiCameraManager : public CameraInterface {
   std::map<int32_t, SourceSurfaceDesc> source_surface_;
   std::map<int32_t, SurfaceCrop> surface_crop_;
 
-  std::vector<uint32_t>    active_streams_;
+  std::set<uint32_t>    active_streams_;
 
   // map of virtual camera id and its corresponding actual camera Ids.
   // <virtual camera id, Vector of actual camera id >
