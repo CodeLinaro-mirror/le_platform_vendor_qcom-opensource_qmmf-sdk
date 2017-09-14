17f87f18b7d7e04dc8b6a47b86089de4100864c4
Konstantin Motov <kmotov@codeaurora.org>
2017-09-14 14:41:50 +0300

    recorder: remove old HAL Jpeg reprocess node
    This functionality is handle by HAL reprocess node.
    
    Change-Id: I3217757225fa4d666be46d8b341c84042d8b0e0a

--- recorder/src/service/post-process/factory/qmmf_postproc_factory.cc
+++ recorder/src/service/post-process/factory/qmmf_postproc_factory.cc
@@ -31,7 +31,6 @@
 
 #include <dirent.h>
 
-#include "../modules/camera-hal-jpeg/qmmf_camera_hal_jpeg.h"
 #include "../modules/camera-hal-reproc/qmmf_camera_hal_reproc.h"
 #include "../modules/jpeg-encoder/qmmf_jpeg.h"
 #include "../modules/test/qmmf_postproc_test.h"
