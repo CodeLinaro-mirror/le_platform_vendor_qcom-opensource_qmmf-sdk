84c5337ae79d9893fbbca35579d1b674f43a4cb3
Amrutha Sai Gattu <agattu@codeaurora.org>
2017-09-23 12:35:29 +0530

    qmmf-sdk: Set low power mode in missing stream configuration paths
    Low Power Mode was not getting set when ConfigureStreamsLocked was
    getting invoked from CreateStream.
    
    Change-Id: I79ee0d5d9cad42d40c5b1c6596d1c9b3c3a34456

--- recorder/src/service/qmmf_camera_context.cc
+++ recorder/src/service/qmmf_camera_context.cc
@@ -166,7 +166,7 @@ status_t CameraContext::CreateSnapshotStream(const ImageParam &param) {
             stream_param.height, stream_param.format);
 
   ret = CreateDeviceStream(stream_param, camera_start_params_.frame_rate,
-                           &stream_id, true);
+                           &stream_id);
   if (ret != NO_ERROR) {
     QMMF_ERROR("%s:%s: Failed creating snapshot stream: %d!",
                TAG, __func__, ret);
@@ -997,8 +997,7 @@ Vector<int32_t>& CameraContext::GetSupportedFps() {
 
 status_t CameraContext::CreateDeviceStream(CameraStreamParameters& params,
                                            uint32_t frame_rate,
-                                           int32_t* stream_id,
-                                           bool is_pp_enabled) {
+                                           int32_t* stream_id) {
 
   Mutex::Autolock lock(device_access_lock_);
   QMMF_VERBOSE("%s:%s: Enter", TAG, __func__);
@@ -1058,7 +1057,7 @@ status_t CameraContext::CreateDeviceStream(CameraStreamParameters& params,
     }
 
     ret = camera_device_->EndConfigure(is_constrained_mode, is_raw_only,
-                                       batch_size_, is_pp_enabled);
+                                       batch_size_, params.is_pp_enabled);
     assert(ret == NO_ERROR);
   }
 
@@ -1888,11 +1887,10 @@ status_t CameraPort::Init() {
   cam_stream_params_.grallocFlags =
       GRALLOC_USAGE_SW_READ_OFTEN | GRALLOC_USAGE_SW_WRITE_OFTEN;
 
-  bool is_pp_enabled = true;
   if (params_.low_power_mode) {
       cam_stream_params_.format = HAL_PIXEL_FORMAT_YCbCr_420_888;
       cam_stream_params_.bufferCount  = PREVIEW_STREAM_BUFFER_COUNT;
-      is_pp_enabled  = false;
+      cam_stream_params_.is_pp_enabled  = false;
   } else {
     cam_stream_params_.grallocFlags |= private_handle_t::
         PRIV_FLAGS_VIDEO_ENCODER;
@@ -1934,8 +1932,7 @@ status_t CameraPort::Init() {
 
   int32_t stream_id;
   auto ret = context_->CreateDeviceStream(cam_stream_params_,
-                                          params_.frame_rate, &stream_id,
-                                          is_pp_enabled);
+                                          params_.frame_rate, &stream_id);
   if (ret != NO_ERROR || stream_id < 0) {
     QMMF_ERROR("%s:%s: CreateDeviceStream failed!!", TAG, __func__);
     return BAD_VALUE;
@@ -2381,8 +2378,7 @@ status_t ZslPort::SetUpZSL() {
 
   ret = context_->CreateDeviceStream(zsl_stream_params,
                                      cam_start_param.frame_rate,
-                                     &camera_stream_id_,
-                                     true);
+                                     &camera_stream_id_);
   if (NO_ERROR != ret || camera_stream_id_ < 0) {
     QMMF_ERROR("%s:%s: CreateDeviceStream failed!", TAG, __func__);
     return ret;
