fd0b961018296c7a79380b82d1c2ca7edf0a0d61
Partha Mitra <pmitra@codeaurora.org>
2017-10-17 16:49:26 +0530

    recorder: Decrease sensitivity of frame skip calculation
    Restore sensitivity of frame skip selection logic to 0.5. This means
    skip parameters calculation will be triggered only when frame rate
    fluctuation from Camera is atleast 0.5.
    
    Change-Id: I5e1092d5e9a075bafad600e281fe4887144d1efa

--- recorder/src/service/qmmf_camera_source.h
+++ recorder/src/service/qmmf_camera_source.h
@@ -53,7 +53,7 @@ using namespace avcodec;
 
 namespace recorder {
 
-#define FPS_CHANGE_THRESHOLD  (0.005)
+#define FPS_CHANGE_THRESHOLD  (0.5)
 #define FRAME_SKIP_THRESHOLD_PERCENT (0.05)
 
 class TrackSource;
