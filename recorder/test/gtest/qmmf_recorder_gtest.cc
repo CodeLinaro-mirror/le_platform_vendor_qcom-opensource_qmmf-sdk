e379f1c163cc677c09c2ccdc1698cb066cc75ee0
Konstantin Motov <kmotov@codeaurora.org>
2017-12-14 20:01:48 +0200

    recorder: gtest: set capture resolution to 10MP (3872x2592)
    Change capture resolution in all post processing test.
    
    Change-Id: I1f825b543df600690f9e67cbea85743da0077d3c

--- recorder/test/gtest/qmmf_recorder_gtest.cc
+++ recorder/test/gtest/qmmf_recorder_gtest.cc
@@ -1551,7 +1551,144 @@ TEST_F(RecorderGtest, 4KSnapshot) {
 }
 
 /*
-* 4KSnapshotWithEdgeSmooth: This test will test 4K JPEG snapshot with
+* 10MPSnapshot: This test will test 10MP JPEG snapshot.
+* Api test sequence:
+*  - StartCamera
+*   loop Start {
+*   ------------------
+*   - CaptureImage - JPEG
+*   ------------------
+*   } loop End
+*  - StopCamera
+*/
+TEST_F(RecorderGtest, 10MPSnapshot) {
+  fprintf(stderr,"\n---------- Run Test %s.%s ------------\n",
+      test_info_->test_case_name(),test_info_->name());
+
+  auto ret = Init();
+  assert(ret == NO_ERROR);
+
+  ret = recorder_.StartCamera(camera_id_, camera_start_params_);
+  assert(ret == NO_ERROR);
+
+  SessionCb session_status_cb;
+  session_status_cb.event_cb = [this] (EventType event_type, void *event_data,
+                                       size_t event_data_size) -> void {
+      SessionCallbackHandler(event_type, event_data, event_data_size); };
+
+  uint32_t session_id;
+  ret = recorder_.CreateSession(session_status_cb, &session_id);
+  assert(session_id > 0);
+  assert(ret == NO_ERROR);
+
+  TrackCb preview_track_cb;
+  preview_track_cb.event_cb =
+      [this] (uint32_t track_id, EventType event_type,
+              void *event_data, size_t event_data_size) -> void {
+      VideoTrackEventCb(track_id, event_type, event_data, event_data_size); };
+
+  uint32_t preview_track_id = 1;
+
+  VideoTrackCreateParam preview_track_param{camera_id_, VideoFormat::kYUV,
+                                          640,
+                                          480,
+                                          30};
+    preview_track_param.low_power_mode = true;
+
+  preview_track_cb.data_cb = [&, session_id] (uint32_t track_id,
+      std::vector<BufferDescriptor> buffers,
+      std::vector<MetaData> meta_buffers) {
+          VideoTrackYUVDataCb(session_id, track_id, buffers, meta_buffers);
+      };
+
+  ret = recorder_.CreateVideoTrack(session_id, preview_track_id,
+                                   preview_track_param, preview_track_cb);
+  assert(ret == NO_ERROR);
+
+  std::vector<uint32_t> track_ids = {preview_track_id};
+  sessions_.insert(std::make_pair(session_id, track_ids));
+
+  ret = recorder_.StartSession(session_id);
+  assert(ret == NO_ERROR);
+
+  // Record for sometime
+  sleep(1);
+
+  ImageParam image_param{};
+  image_param.width         = 3872;
+  image_param.height        = 2592;
+  image_param.image_format  = ImageFormat::kJPEG;
+  image_param.image_quality = 95;
+
+  std::vector<CameraMetadata> meta_array;
+  camera_metadata_entry_t entry;
+  CameraMetadata meta;
+
+  ret = recorder_.GetDefaultCaptureParam(camera_id_, meta);
+  assert(ret == NO_ERROR);
+
+  meta_array.push_back(meta);
+
+  bool res_supported = false;
+  // Check Supported JPEG snapshot resolutions.
+  if (meta.exists(ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS)) {
+    entry = meta.find(ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS);
+    for (uint32_t i = 0 ; i < entry.count; i += 4) {
+      if (HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED == entry.data.i32[i]) {
+        if (ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT ==
+            entry.data.i32[i+3]) {
+          if (image_param.width == static_cast<uint32_t>(entry.data.i32[i+1])
+              && image_param.height ==
+                  static_cast<uint32_t>(entry.data.i32[i+2])) {
+            res_supported = true; // 3840x2160 JPEG supported.
+          }
+        }
+      }
+    }
+  }
+  assert (res_supported != false);
+
+  ImageCaptureCb cb = [this] (uint32_t camera_id, uint32_t image_count,
+                              BufferDescriptor buffer,
+                              MetaData meta_data) -> void
+      { SnapshotCb(camera_id, image_count, buffer, meta_data); };
+
+  for (uint32_t i = 1; i <= iteration_count_; i++) {
+    fprintf(stderr,"test iteration = %d/%d\n", i, iteration_count_);
+    TEST_INFO("%s: Running Test(%s) iteration = %d ", __func__,
+        test_info_->name(), i);
+    ret = recorder_.CaptureImage(camera_id_, image_param, 1, meta_array, cb);
+    assert(ret == NO_ERROR);
+    // Take snapshot after every 5 sec.
+    sleep(5);
+  }
+
+  ret = recorder_.CancelCaptureImage(camera_id_);
+  assert(ret == NO_ERROR);
+
+  ret = recorder_.StopSession(session_id, false);
+  assert(ret == NO_ERROR);
+
+  ret = recorder_.DeleteVideoTrack(session_id, preview_track_id);
+  assert(ret == NO_ERROR);
+
+  ret = recorder_.DeleteSession(session_id);
+  assert(ret == NO_ERROR);
+
+  ClearSessions();
+
+  ret = recorder_.StopCamera(camera_id_);
+  assert(ret == NO_ERROR);
+
+  ret = DeInit();
+  assert(ret == NO_ERROR);
+
+  fprintf(stderr,"---------- Test Completed %s.%s ----------\n",
+      test_info_->test_case_name(), test_info_->name());
+}
+
+/*
+* 10MPSnapshotWithEdgeSmooth: This test will test 10MP JPEG snapshot with
 *                       reprocessing. Reprocessing pipe is EdgeSmooth and JPEG.
 * Api test sequence:
 *  - StartCamera
@@ -1562,7 +1699,7 @@ TEST_F(RecorderGtest, 4KSnapshot) {
 *   } loop End
 *  - StopCamera
 */
-TEST_F(RecorderGtest, 4KSnapshotWithEdgeSmooth) {
+TEST_F(RecorderGtest, 10MPSnapshotWithEdgeSmooth) {
   fprintf(stderr,"\n---------- Run Test %s.%s ------------\n",
       test_info_->test_case_name(),test_info_->name());
 
@@ -1615,8 +1752,8 @@ TEST_F(RecorderGtest, 4KSnapshotWithEdgeSmooth) {
   sleep(1);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -1713,7 +1850,7 @@ TEST_F(RecorderGtest, 4KSnapshotWithEdgeSmooth) {
 }
 
 /*
-* 4KSnapshotWithLCAC: This test will test 4K JPEG snapshot with
+* 10MPSnapshotWithLCAC: This test will test 10MP JPEG snapshot with
 *                     reprocessing. Reprocessing pipe is bayer LCAC,
 *                     bayer to you reprocessing and JPEG.
 * Api test sequence:
@@ -1725,7 +1862,7 @@ TEST_F(RecorderGtest, 4KSnapshotWithEdgeSmooth) {
 *   } loop End
 *  - StopCamera
 */
-TEST_F(RecorderGtest, 4KSnapshotWithLCAC) {
+TEST_F(RecorderGtest, 10MPSnapshotWithLCAC) {
   fprintf(stderr,"\n---------- Run Test %s.%s ------------\n",
       test_info_->test_case_name(),test_info_->name());
 
@@ -1779,8 +1916,8 @@ TEST_F(RecorderGtest, 4KSnapshotWithLCAC) {
   sleep(1);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -1876,7 +2013,7 @@ TEST_F(RecorderGtest, 4KSnapshotWithLCAC) {
 }
 
 /*
-* 4KSnapshotWithLCACandEdgeSmooth: This test will test 4K JPEG snapshot with
+* 10MPSnapshotWithLCACandEdgeSmooth: This test will test 10MP JPEG snapshot with
 *                     reprocessing. Reprocessing pipe is bayer LCAC,
 *                     bayer to you reprocessing, edge smooth and JPEG.
 * Api test sequence:
@@ -1888,7 +2025,7 @@ TEST_F(RecorderGtest, 4KSnapshotWithLCAC) {
 *   } loop End
 *  - StopCamera
 */
-TEST_F(RecorderGtest, 4KSnapshotWithLCACandEdgeSmooth) {
+TEST_F(RecorderGtest, 10MPSnapshotWithLCACandEdgeSmooth) {
   fprintf(stderr,"\n---------- Run Test %s.%s ------------\n",
       test_info_->test_case_name(),test_info_->name());
 
@@ -1941,8 +2078,8 @@ TEST_F(RecorderGtest, 4KSnapshotWithLCACandEdgeSmooth) {
   sleep(1);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -2166,7 +2303,7 @@ TEST_F(RecorderGtest, BurstSnapshotWithThumbnails) {
       test_info_->test_case_name(), test_info_->name());
 }
 /*
-* BurstSnapshot: This test will test 4K Burst jpg snapshot.
+* BurstSnapshot: This test will test 10MP Burst jpg snapshot.
 * Api test sequence:
 *  - StartCamera
 *  - CaptureImage - Burst Jpg
@@ -2184,8 +2321,8 @@ TEST_F(RecorderGtest, BurstSnapshot) {
   ASSERT_TRUE(ret == NO_ERROR);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -2337,8 +2474,8 @@ TEST_F(RecorderGtest, BurstSnapshotWithYuvCAC) {
   sleep(1);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -2530,8 +2667,8 @@ TEST_F(RecorderGtest, BurstSnapshotWithBayerLCAC) {
   sleep(1);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -2745,8 +2882,8 @@ TEST_F(RecorderGtest, BurstSnapshotWithBayerLCAC15fps) {
   sleep(2);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
@@ -3011,8 +3148,8 @@ TEST_F(RecorderGtest, AutoBurstCaptureWithBayerLCAC) {
     sleep(2);
 
     ImageParam image_param{};
-    image_param.width         = 3840;
-    image_param.height        = 2160;
+    image_param.width         = 3872;
+    image_param.height        = 2592;
     image_param.image_format  = ImageFormat::kJPEG;
     image_param.image_quality = default_jpeg_quality_;
 
@@ -3278,8 +3415,8 @@ TEST_F(RecorderGtest, ContinuousSnapshotWithBayerLCAC) {
   sleep(2);
 
   ImageParam image_param{};
-  image_param.width         = 3840;
-  image_param.height        = 2160;
+  image_param.width         = 3872;
+  image_param.height        = 2592;
   image_param.image_format  = ImageFormat::kJPEG;
   image_param.image_quality = default_jpeg_quality_;
 
