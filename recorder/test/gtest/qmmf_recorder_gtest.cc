10fa5f38ab4f54b97eef7b58ac40248396dd7166
Sinjan Kumar <sinjank@codeaurora.org>
2017-12-14 17:22:43 +0530

    recorder_gtest: Modify cross platform open() call signature
    open() call is expecting 3 arguments in LA, 3rd one is for mode:
    
     int open(const char* const __pass_object_size pathname,
              int flags, mode_t modes)
    
    Change-Id: I3e9b12692f080652cb4fec4847adcd4708101b07

--- recorder/test/gtest/qmmf_recorder_gtest.cc
+++ recorder/test/gtest/qmmf_recorder_gtest.cc
@@ -9965,8 +9965,7 @@ TEST_F(RecorderGtest, SingleSessionCameraParamTest) {
 
   // run session for some time
   sleep(5);
-
-  int dump_fd = open(DUMP_META_PATH, O_WRONLY|O_CREAT);
+  int dump_fd = open(DUMP_META_PATH, O_WRONLY|O_CREAT, 0644);
   assert(0 <= dump_fd);
 
   CameraMetadata meta;
@@ -17343,7 +17342,7 @@ status_t DumpBitStream::SetUp(const StreamDumpInfo& dumpinfo) {
   bitstream_filepath += std::to_string(dumpinfo.height) + ".";
   bitstream_filepath += extn;
   int32_t file_fd = open(bitstream_filepath.c_str(),
-                          O_CREAT | O_WRONLY | O_TRUNC, 0655);
+                          O_CREAT | O_WRONLY | O_TRUNC, 0644);
   if (file_fd <= 0) {
     TEST_ERROR("%s File open failed!", __func__);
     return BAD_VALUE;
