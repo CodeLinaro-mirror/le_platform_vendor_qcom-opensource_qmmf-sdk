52742dcc7d79f20570c4d0f22ff6361d760c4852
Sagar Sahu <sahusag@codeaurora.org>
2018-02-24 02:59:27 +0530

    recorder_gtest: Add concurrency gtest for QCS605
    - SessionWithDualCam5_7k30Enc1080p30EncAndLinked1080p30YUVWithTNRAndZZHDR
    - SessionWithDualCam4k60Enc1080p30EncAndLinked1080p30YUVWithTNRAndZZHDR
    - SessionWithDualCam4k30Enc1080p30EncAndLinked1080p30YUVWithTNRAndZZHDR
    
    Change-Id: Iba8492680471eae6cbbc6b3d84e914765be322af

--- recorder/test/gtest/qmmf_recorder_gtest.h
+++ recorder/test/gtest/qmmf_recorder_gtest.h
@@ -321,6 +321,13 @@ class RecorderGtest : public ::testing::Test {
 
   bool VendorTagExistsInMeta(const CameraMetadata& meta, const String8& name,
                              const String8& section, uint32_t* tag_id);
+
+  void CreatePrivacyMaskOverlay(const uint32_t& video_track_id,
+                                const int32_t& width, const int32_t& height,
+                                uint32_t* mask_id);
+
+  void DestroyPrivacyMaskOverlay (const uint32_t& video_track_id,
+                                  const uint32_t& mask_id);
 #endif
 
   Recorder              recorder_;
