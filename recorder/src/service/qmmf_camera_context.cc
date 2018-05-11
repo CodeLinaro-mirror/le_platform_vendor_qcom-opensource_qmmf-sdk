1507b0320089c360e0c8d453644e500ce74dff7b
Petar Georgiev <petarg@codeaurora.org>
2018-05-10 21:23:59 -0700

    recorder: Fix camera flush and CancelCaptureImage
    Fix state hard coded state transition while flush.
    Wait until idle after flush.
    Return buffers immediately after post process abort.
    
    Change-Id: Ib5605bfeb48848b5e471014025e97b1521d34120

--- recorder/src/service/qmmf_camera_context.cc
+++ recorder/src/service/qmmf_camera_context.cc
@@ -928,7 +928,6 @@ status_t CameraContext::CancelCaptureImage() {
     PauseActiveStreams();
     PostProcDelete();
     DeleteSnapshotStream();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
     ResumeActiveStreams();
   }
 
@@ -1079,8 +1078,7 @@ status_t CameraContext::CreateStream(const CameraStreamParam& param,
     ret = CreateCaptureRequest(streaming_active_requests_[0],
                                CAMERA3_TEMPLATE_VIDEO_RECORD);
     assert(ret == NO_ERROR);
-    QMMF_INFO("%s: Global Streaming Capture request created successfully!",
-        __func__);
+    QMMF_INFO("%s: Global Streaming request created successfully!",__func__);
   }
 
   // Add port to list of active ports.
@@ -1799,6 +1797,9 @@ status_t CameraContext::PauseActiveStreams(bool immedialtely) {
     std::lock_guard<std::mutex> lock(device_access_lock_);
     int64_t last_frame_mumber;
     ret = camera_device_->Flush(&last_frame_mumber);
+    assert(ret == NO_ERROR);
+
+    ret = camera_device_->WaitUntilIdle();
     assert(ret == NO_ERROR);
 
     // inform all active ports that streaming is interrupted
