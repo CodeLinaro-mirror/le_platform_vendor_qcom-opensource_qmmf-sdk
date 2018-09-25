247e5887aa4111896e928c929b1d276062c46832
Amrutha Sai Gattu <agattu@codeaurora.org>
2018-09-25 16:09:05 +0530

    qmmf_sdk: include qmmf_log.h in qmmf_server_main.cc
    - Replace ALOGD with QMMF_DEBUG
    
    Change-Id: Ibaf8536c73bea6d3f370916212e484d1b01e932a

--- qmmf-server/qmmf_server_main.cc
+++ qmmf-server/qmmf_server_main.cc
@@ -36,6 +36,7 @@
 #include <binder/IPCThreadState.h>
 #include <cutils/properties.h>
 
+#include "common/utils/qmmf_log.h"
 #include "recorder/src/service/qmmf_recorder_service.h"
 #include "common/audio/src/service/qmmf_audio_service.h"
 #include "player/src/service/qmmf_player_service.h"
@@ -56,7 +57,7 @@ using namespace system;
   do { \
     printf(__VA_ARGS__); \
     printf("\n"); \
-    ALOGD(__VA_ARGS__); \
+    QMMF_DEBUG(__VA_ARGS__); \
 } while(0)
 
 uint32_t qmmf_log_level;
