1105963b4135119224030611c7b4596e29b23ac6
Anton Dimitrov <antond@codeaurora.org>
2018-11-16 12:59:14 +0200

    qmmf-sdk: qmmf-algo: Remove Get caps api from algo interface
     - Each lib will return extern Get caps "C" function
    
    Change-Id: I42aef4ef4e2b8de4cfb0b0802531b058657c727e

--- recorder/src/service/post-process/modules/algo/qmmf_postproc_algo.cc
+++ recorder/src/service/post-process/modules/algo/qmmf_postproc_algo.cc
@@ -71,7 +71,9 @@ PostProcAlg::PostProcAlg(std::string lib)
   pass_through_ = (0 == atoi(prop_val)) ? false : true;
 
   try {
-    algo_caps_ = algo_->GetCaps();
+    QmmfAlgGetCapabilites GetCapsFunc;
+    Utils::LoadLibHandler(lib_handle_, QMMF_ALG_GET_CAPS_FUNC, GetCapsFunc);
+    algo_caps_ = *GetCapsFunc();
   } catch (const std::exception &e) {
     QMMF_ERROR("%s: Error getting capabilities, exception: %s", __func__,
         e.what());
@@ -349,7 +351,8 @@ status_t PostProcAlg::Process(
       return BAD_VALUE;
     }
 
-    assert(in_alg_buffers.size() == algo_caps_.out_buffer_requirements_.count_);
+    assert(in_alg_buffers.size() ==
+           algo_caps_.out_buffer_requirements_.count_);
     if (dump_in_frame_ == true) {
       for (auto buf : in_alg_buffers) {
         DumpFrame(buf, true);
