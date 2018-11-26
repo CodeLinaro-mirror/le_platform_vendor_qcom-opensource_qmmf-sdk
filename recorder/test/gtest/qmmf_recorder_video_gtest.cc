080d78d6d6c0eaa7ea3c4e0dbf382a9c0a9fc6f8
jshiv <jshiv@codeaurora.org>
2018-11-26 18:01:16 +0530

    recorder_gtest: add gtest to test anr and tnr feature.
    following gtest has been added:
    
    SessionWith4kEncWithTNRModes
    SessionWith4kEncWithANRModes
    
    Change-Id: Ib9b9fea2021e54111f52f5bcba626fe8301f8b57

--- recorder/test/gtest/qmmf_recorder_video_gtest.cc
+++ recorder/test/gtest/qmmf_recorder_video_gtest.cc
@@ -11213,4 +11213,299 @@ TEST_F(VideoGtest, SessionWithDualCam5_7k30EncRescale1080p30EncAnd1080p30YUVWith
       test_info_->test_case_name(), test_info_->name());
 }
 
+/*
+* SessionWith4kEncWithTNRModes: This case will test a  session with
+*                      4k h264 encoded track with TNR modes changes.
+* API test sequence:
+*   StartCamera
+*   loop Start {
+*   --------------------------
+*   - CreateSession
+*   - CreateVideoTrack
+*   - StartVideoTrack
+*   - StartSession
+*   - Set TNR Mode
+*   - StopSession
+*   - DeleteVideoTrack
+*   - DeleteSession
+*   --------------------------
+*   } loop End
+*  StopCamera
+*/
+TEST_F(VideoGtest, SessionWith4kEncWithTNRModes) {
+  fprintf(stderr, "\n---------- Run Test %s.%s ------------\n",
+          test_info_->test_case_name(), test_info_->name());
+
+  auto ret = Init();
+  ASSERT_TRUE(ret == NO_ERROR);
+  camera_start_params_.frame_rate = 30;
+  ret = recorder_.StartCamera(camera_id_, camera_start_params_);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  VideoFormat format_type = VideoFormat::kAVC;
+  uint32_t stream_width = 3840;
+  uint32_t stream_height = 2160;
+  uint32_t video_track_id = 1;
+
+  float min_tnr_range, max_tnr_range;
+  for (uint32_t i = 1; i <= iteration_count_; i++) {
+    TEST_INFO("%s: Running Test(%s) iteration = %d ", __func__,
+              test_info_->name(), i);
+
+    SessionCb session_status_cb = CreateSessionStatusCb();
+    uint32_t session_id;
+    ret = recorder_.CreateSession(session_status_cb, &session_id);
+    ASSERT_TRUE(session_id > 0);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    if (dump_bitstream_.IsEnabled()) {
+      StreamDumpInfo dumpinfo = {format_type, session_id, video_track_id,
+                                 stream_width, stream_height};
+      ret = dump_bitstream_.SetUp(dumpinfo);
+      ASSERT_TRUE(ret == NO_ERROR);
+    }
+
+    VideoTrackCreateParam video_track_param{camera_id_, format_type,
+                                            stream_width, stream_height, 30};
+
+    TrackCb video_track_cb;
+    video_track_cb.data_cb = [&, session_id](
+        uint32_t track_id, std::vector<BufferDescriptor> buffers,
+        std::vector<MetaData> meta_buffers) {
+      VideoTrackEncDataCb(session_id, track_id, buffers, meta_buffers);
+    };
+
+    video_track_cb.event_cb = [&](uint32_t track_id, EventType event_type,
+                                  void *event_data, size_t event_data_size) {
+      VideoTrackEventCb(track_id, event_type, event_data, event_data_size);
+    };
+
+    ret = recorder_.CreateVideoTrack(session_id, video_track_id,
+                                     video_track_param, video_track_cb);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.StartSession(session_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    CameraMetadata meta;
+    ret = recorder_.GetCameraParam(camera_id_, meta);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    uint32_t tnr_intensity_vtag, tnr_blend_strength_vtag,
+        motion_detection_sensitivity_vtag, tnr_tuning_range_vtag;
+
+    if (!VendorTagSupported(String8("tnr_intensity"),
+                            String8("org.codeaurora.qcamera3.tnr_tuning"),
+                            &tnr_intensity_vtag)) {
+      TEST_ERROR("%s: tnr_intensity_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+    if (!VendorTagSupported(String8("tnr_blend_strength"),
+                            String8("org.codeaurora.qcamera3.tnr_tuning"),
+                            &tnr_blend_strength_vtag)) {
+      TEST_ERROR("%s: tnr_blend_strength_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+    if (!VendorTagSupported(String8("motion_detection_sensitivity"),
+                            String8("org.codeaurora.qcamera3.tnr_tuning"),
+                            &motion_detection_sensitivity_vtag)) {
+      TEST_ERROR("%s: motion_detection_sensitivity_vtag is not supported",
+                 __func__);
+      ASSERT_TRUE(0);
+    }
+    if (VendorTagSupported(String8("tnr_tuning_range"),
+                           String8("org.codeaurora.qcamera3.tnr_tuning"),
+                           &tnr_tuning_range_vtag)) {
+      auto entry = meta.find(tnr_tuning_range_vtag);
+      min_tnr_range = entry.data.f[0];
+      max_tnr_range = entry.data.f[1];
+      fprintf(stderr, "min_tnr_range = %f, max_tnr_range = %f\n",
+              min_tnr_range, max_tnr_range);
+    } else {
+      TEST_ERROR("%s: tnr_tuning_range_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+
+    for (float count = min_tnr_range + 1; count < max_tnr_range; count += 20) {
+      float value = count;
+
+      ret = meta.update(tnr_intensity_vtag, &value, 1);
+      ASSERT_TRUE(ret == NO_ERROR);
+      ret = meta.update(tnr_blend_strength_vtag, &value, 1);
+      ASSERT_TRUE(ret == NO_ERROR);
+      ret = meta.update(motion_detection_sensitivity_vtag, &value, 1);
+      ASSERT_TRUE(ret == NO_ERROR);
+
+      fprintf(stderr, "TNR values are getting changed to [%f]\n", count);
+      ret = recorder_.SetCameraParam(camera_id_, meta);
+      ASSERT_TRUE(ret == NO_ERROR);
+
+      sleep(record_duration_ / 10);
+    }
+
+    ret = recorder_.StopSession(session_id, false);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.DeleteVideoTrack(session_id, video_track_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.DeleteSession(session_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    dump_bitstream_.CloseAll();
+  }
+
+  ret = recorder_.StopCamera(camera_id_);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  ret = DeInit();
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  fprintf(stderr, "---------- Test Completed %s.%s ----------\n",
+          test_info_->test_case_name(), test_info_->name());
+}
+
+/*
+* SessionWith4kEncWithANRModes: This case will test a  session with
+*                      4k h264 encoded track with ANR modes changes.
+* API test sequence:
+*   StartCamera
+*   loop Start {
+*   --------------------------
+*   - CreateSession
+*   - CreateVideoTrack
+*   - StartVideoTrack
+*   - StartSession
+*   - Set ANR Mode
+*   - StopSession
+*   - DeleteVideoTrack
+*   - DeleteSession
+*   --------------------------
+*   } loop End
+*  StopCamera
+*/
+TEST_F(VideoGtest, SessionWith4kEncWithANRModes) {
+  fprintf(stderr, "\n---------- Run Test %s.%s ------------\n",
+          test_info_->test_case_name(), test_info_->name());
+
+  auto ret = Init();
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  ret = recorder_.StartCamera(camera_id_, camera_start_params_);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  VideoFormat format_type = VideoFormat::kAVC;
+  uint32_t stream_width = 3840;
+  uint32_t stream_height = 2160;
+  uint32_t video_track_id = 1;
+
+  float min_anr_range, max_anr_range;
+  for (uint32_t i = 1; i <= iteration_count_; i++) {
+    TEST_INFO("%s: Running Test(%s) iteration = %d ", __func__,
+              test_info_->name(), i);
+
+    SessionCb session_status_cb = CreateSessionStatusCb();
+    uint32_t session_id;
+    ret = recorder_.CreateSession(session_status_cb, &session_id);
+    ASSERT_TRUE(session_id > 0);
+    ASSERT_TRUE(ret == NO_ERROR);
+    VideoTrackCreateParam video_track_param{camera_id_, format_type,
+                                            stream_width, stream_height, 30};
+
+    if (dump_bitstream_.IsEnabled()) {
+      StreamDumpInfo dumpinfo = {format_type, session_id, video_track_id,
+                                 stream_width, stream_height};
+      ret = dump_bitstream_.SetUp(dumpinfo);
+      ASSERT_TRUE(ret == NO_ERROR);
+    }
+
+    TrackCb video_track_cb;
+    video_track_cb.data_cb = [&, session_id](
+        uint32_t track_id, std::vector<BufferDescriptor> buffers,
+        std::vector<MetaData> meta_buffers) {
+      VideoTrackEncDataCb(session_id, track_id, buffers, meta_buffers);
+    };
+
+    video_track_cb.event_cb = [&](uint32_t track_id, EventType event_type,
+                                  void *event_data, size_t event_data_size) {
+      VideoTrackEventCb(track_id, event_type, event_data, event_data_size);
+    };
+
+    ret = recorder_.CreateVideoTrack(session_id, video_track_id,
+                                     video_track_param, video_track_cb);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.StartSession(session_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    CameraMetadata meta;
+    ret = recorder_.GetCameraParam(camera_id_, meta);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    uint32_t anr_intensity_vtag, anr_motion_sensitivity_vtag,
+        anr_tuning_range_vtag;
+
+    if (!VendorTagSupported(String8("anr_intensity"),
+                            String8("org.quic.camera.anr_tuning"),
+                            &anr_intensity_vtag)) {
+      TEST_ERROR("%s: anr_intensity_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+    if (!VendorTagSupported(String8("anr_motion_sensitivity"),
+                            String8("org.quic.camera.anr_tuning"),
+                            &anr_motion_sensitivity_vtag)) {
+      TEST_ERROR("%s: anr_motion_sensitivity_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+
+    if (VendorTagSupported(String8("anr_tuning_range"),
+                           String8("org.quic.camera.anr_tuning"),
+                           &anr_tuning_range_vtag)) {
+      auto entry = meta.find(anr_tuning_range_vtag);
+      min_anr_range = entry.data.f[0];
+      max_anr_range = entry.data.f[1];
+      fprintf(stderr, "min_anr_range = %f , max_anr_range = %f \n",
+              min_anr_range, max_anr_range);
+    } else {
+      TEST_ERROR("%s: anr_tuning_range_vtag is not supported", __func__);
+      ASSERT_TRUE(0);
+    }
+
+    for (float count = min_anr_range + 1; count < max_anr_range; count += 20) {
+      float value = count;
+
+      ret = meta.update(anr_intensity_vtag, &value, 1);
+      ASSERT_TRUE(ret == NO_ERROR);
+      ret = meta.update(anr_motion_sensitivity_vtag, &value, 1);
+      ASSERT_TRUE(ret == NO_ERROR);
+
+      fprintf(stderr, "ANR values are getting changed to [%f]\n", count);
+      ret = recorder_.SetCameraParam(camera_id_, meta);
+      ASSERT_TRUE(ret == NO_ERROR);
+
+      sleep(record_duration_ / 10);
+    }
+
+    ret = recorder_.StopSession(session_id, false);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.DeleteVideoTrack(session_id, video_track_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    ret = recorder_.DeleteSession(session_id);
+    ASSERT_TRUE(ret == NO_ERROR);
+
+    dump_bitstream_.CloseAll();
+  }
+
+  ret = recorder_.StopCamera(camera_id_);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  ret = DeInit();
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  fprintf(stderr, "---------- Test Completed %s.%s ----------\n",
+          test_info_->test_case_name(), test_info_->name());
+}
+
 #endif
