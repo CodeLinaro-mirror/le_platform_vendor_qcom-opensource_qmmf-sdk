4e44448a9dde68eb43bf27bd0e9dfbaee816fe38
Angel Hitov <ahitov@codeaurora.org>
2018-11-27 17:32:03 +0200

    qmmf-sdk: qmmf-algs: Export Algs gtest json data files
    Algorithm gtests use external json data files. They need to be exported
    in /data/misc/qmmf/
    
    Change-Id: Idb2f03e939a457dbbf31b34316a5dc3e213c585d

--- plugins/Android.mk
+++ plugins/Android.mk
@@ -122,4 +122,148 @@ endif
 
 include $(BUILD_NATIVE_TEST)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_bayer_lcac.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_binning_correction.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_bit_blit.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_edge_smooth.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_inplace_history.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_inplace.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_outplace_history.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_outplace.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_privacy_mask.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_res_conv_sub.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_svhdr.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qmmf_algo_gtest_yuv_cac.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bayer_lcac_calibration.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := binning_correction_calibration.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bit_blit_configuration.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privacy_mask_configuration.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := res_conv_sub_configuration.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := yuv_cac_calibration_nv21.json
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/misc/qmmf/
+LOCAL_SRC_FILES := test_cases/calibration/$(LOCAL_MODULE)
+LOCAL_MODULE_OWNER := qti
+include $(BUILD_PREBUILT)
+
 endif # BUILD_QMMMF
