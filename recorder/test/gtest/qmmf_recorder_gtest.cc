a2112265a5100587a00923790e69b5a55399db01
Ajit Singh Solanki <ajits@codeaurora.org>
2019-01-14 16:00:52 -0800

    recorder_gtest: Update gtest to fix multiple issues.
    - Remove EdgeSmooth disablement logic after taking 1st
      capture before going to continuous capture mode.
    - Change default burst count to 15. burst count 15 is good
      engough to test burst capture usecase. count 30 can cause
      LMK in 1 GB device.
    - Tear down EdgeSmooth plugin after taking snapshot.
    
    Change-Id: Iead15aedefadcd28f13fb4cba750d2af83a7d610

--- recorder/test/gtest/qmmf_recorder_gtest.cc
+++ recorder/test/gtest/qmmf_recorder_gtest.cc
@@ -2982,7 +2982,9 @@ TEST_F(RecorderGtest, 10MPSnapshotWithLCACandEdgeSmooth) {
 *  - CancelCaptureImage
 *  - StopCamera
 */
-TEST_F(RecorderGtest, LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapture) {
+TEST_F(RecorderGtest,
+    LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapture) {
+
   fprintf(stderr,"\n---------- Run Test %s.%s ------------\n",
       test_info_->test_case_name(),test_info_->name());
 
@@ -3040,7 +3042,7 @@ TEST_F(RecorderGtest, LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapt
   ASSERT_TRUE(ret == NO_ERROR);
 
   // Record for sometime
-  sleep(5);
+  sleep(3);
 
   ImageParam image_param{};
   image_param.width         = 3872;
@@ -3078,14 +3080,11 @@ TEST_F(RecorderGtest, LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapt
   }
   ASSERT_TRUE(res_supported != false);
 
-  // number of frames 1. Timeout 5s (4fps snapshot).
-  test_wait_.Reset(1, 5);
-
   ImageCaptureCb cb = [this] (uint32_t camera_id, uint32_t image_count,
                               BufferDescriptor buffer,
                               MetaData meta_data) -> void
-      { SnapshotCb(camera_id, image_count, buffer, meta_data);
-        test_wait_.Done();
+      {
+        SnapshotCb(camera_id, image_count, buffer, meta_data);
       };
 
   ImageConfigParam image_config;
@@ -3124,27 +3123,13 @@ TEST_F(RecorderGtest, LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapt
   ret = SetCameraFocalLength(focal_length);
   ASSERT_TRUE(ret == NO_ERROR);
 
-  ret = recorder_.ConfigImageCapture(camera_id_, image_config);
-  ASSERT_TRUE(ret == NO_ERROR);
-
   meta_array.push_back(meta);
-  ret = recorder_.CaptureImage(camera_id_, image_param, 1, meta_array, cb);
-  ASSERT_TRUE(ret == NO_ERROR);
 
-  ret = test_wait_.Wait();
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  //Continius capture
-  std::string config = "{\"EdgeSmooth\" : false }";
-  ret = recorder_.ConfigPlugin(edge_smooth_plugin.uid, config);
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  ImageConfigParam image_config_continius;
   SnapshotType snapshot_type;
   snapshot_type.type = SnapshotMode::kContinuous;
-  image_config_continius.Update(QMMF_SNAPSHOT_TYPE, snapshot_type, 0);
+  image_config.Update(QMMF_SNAPSHOT_TYPE, snapshot_type, 0);
 
-  ret = recorder_.ConfigImageCapture(camera_id_, image_config_continius);
+  ret = recorder_.ConfigImageCapture(camera_id_, image_config);
   ASSERT_TRUE(ret == NO_ERROR);
 
   // Lock AE for snapshot
@@ -3177,10 +3162,13 @@ TEST_F(RecorderGtest, LowResVideo10MPSnapshotWithLCACandEdgeSmoothContinuousCapt
   ret = recorder_.SetCameraParam(camera_id_, meta);
   ASSERT_TRUE(ret == NO_ERROR);
 
-  //preview
+  // Switch back to preview.
   sleep(5);
 
   ret = recorder_.DeletePlugin(bayer_lcac_plugin.uid);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  ret = recorder_.DeletePlugin(edge_smooth_plugin.uid);
   ASSERT_TRUE(ret == NO_ERROR);
 
   ret = recorder_.StopSession(session_id, false);
@@ -3412,8 +3400,8 @@ TEST_F(RecorderGtest, LowResVideo10MPContinuousSnapshotWithLCAC) {
 }
 
 /*
-* LowResVideo10MPContinuousSnapshotWithLCACAndCdsOff: This gtest will test Continuous
-*    10MP JPEG snapshot with Bayer LCAC.
+* LowResVideo10MPContinuousSnapshotWithLCACAndCdsOff: This gtest will test
+*    Continuous 10MP JPEG snapshot with Bayer LCAC.
 * Api test sequence:
 *  - StartCamera
 *  - Low resolution video 640x480@30fps
@@ -3549,7 +3537,6 @@ TEST_F(RecorderGtest, LowResVideo10MPContinuousSnapshotWithLCACAndCdsOff) {
   focal_length = 8.0; // 4 fps mode.
   ret = SetCameraFocalLength(focal_length);
   ASSERT_TRUE(ret == NO_ERROR);
-
 
   SnapshotType snapshot_type;
   snapshot_type.type = SnapshotMode::kContinuous;
@@ -27932,14 +27919,11 @@ TEST_F(RecorderGtest,
   }
   ASSERT_TRUE(res_supported != false);
 
-  // number of frames 1. Timeout 5s (4fps snapshot).
-  test_wait_.Reset(1, 5);
-
   ImageCaptureCb cb = [this] (uint32_t camera_id, uint32_t image_count,
                               BufferDescriptor buffer,
                               MetaData meta_data) -> void
-      { SnapshotCb(camera_id, image_count, buffer, meta_data);
-        test_wait_.Done();
+      {
+        SnapshotCb(camera_id, image_count, buffer, meta_data);
       };
 
   ImageConfigParam image_config;
@@ -27977,32 +27961,16 @@ TEST_F(RecorderGtest,
   high_quality_setup.jpeg_input_format = BufferFormat::kNV16;
   image_config.Update(QMMF_JPEG_CAPTURE_SETUP, high_quality_setup);
 
+  SnapshotType snapshot_type;
+  snapshot_type.type = SnapshotMode::kContinuous;
+  image_config.Update(QMMF_SNAPSHOT_TYPE, snapshot_type, 0);
+
   // Update same focal length to streaming meta.
   focal_length = 8.0;
   ret = SetCameraFocalLength(focal_length);
   ASSERT_TRUE(ret == NO_ERROR);
 
   ret = recorder_.ConfigImageCapture(camera_id_, image_config);
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  meta_array.push_back(meta);
-  ret = recorder_.CaptureImage(camera_id_, image_param, 1, meta_array, cb);
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  ret = test_wait_.Wait();
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  //Continius capture
-  std::string config = "{\"EdgeSmooth\" : false }";
-  ret = recorder_.ConfigPlugin(edge_smooth_plugin.uid, config);
-  ASSERT_TRUE(ret == NO_ERROR);
-
-  ImageConfigParam image_config_continius;
-  SnapshotType snapshot_type;
-  snapshot_type.type = SnapshotMode::kContinuous;
-  image_config_continius.Update(QMMF_SNAPSHOT_TYPE, snapshot_type, 0);
-
-  ret = recorder_.ConfigImageCapture(camera_id_, image_config_continius);
   ASSERT_TRUE(ret == NO_ERROR);
 
   // Lock AE for snapshot
@@ -28039,6 +28007,9 @@ TEST_F(RecorderGtest,
   sleep(5);
 
   ret = recorder_.DeletePlugin(bayer_lcac_plugin.uid);
+  ASSERT_TRUE(ret == NO_ERROR);
+
+  ret = recorder_.DeletePlugin(edge_smooth_plugin.uid);
   ASSERT_TRUE(ret == NO_ERROR);
 
   ret = recorder_.StopSession(session_id, false);
