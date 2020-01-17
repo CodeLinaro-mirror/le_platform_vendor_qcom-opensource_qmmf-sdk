5ef37122c5983b4be5db513dd2c758b15bd9b2ad
Jai Shiv <jshiv@codeaurora.org>
2020-01-17 10:16:13 +0530

    recorder: Fix issue for snapshot stream reconfig
    Issue: When client takes JPEG snapshot and  NV12 snapshot
           back to back, then extension of both the captures
           is returned as jpeg if the resolution of the both
           the streams are same.
    
    Fix:   Allow stream re-config when the format is different
           and resolution is same.
    
    Change-Id: Ia36d93a845adddf46a59776b5c7c2a44fbc92c66

--- recorder/src/service/qmmf_camera_context.cc
+++ recorder/src/service/qmmf_camera_context.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -604,7 +604,9 @@ status_t CameraContext::SetUpCapture(const SnapshotParam& param,
                            (postproc_enable_ != new_postproc_enable) ||
                            IsNeedReconfigSnapshotStream() ||
                            (new_postproc_enable && restart_pipe_) ||
-                           (jpeg_input_format_ != new_jpeg_input_format_);
+                           (jpeg_input_format_ != new_jpeg_input_format_) ||
+                           (snapshot_param_.format != param.format);
+
       QMMF_DEBUG("%s: reconfigure_needed=%d", __func__, reconfigure_needed);
 
       if ((postproc_enable_ && !new_postproc_enable) || reconfigure_needed) {
