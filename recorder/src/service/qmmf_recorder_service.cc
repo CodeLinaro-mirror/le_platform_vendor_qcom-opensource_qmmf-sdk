73131ecffeb97d7405f5f8cd105a9d1df8340e37
Petar G. Georgiev <petarg@codeaurora.org>
2020-09-09 08:55:20 +0300

    recorder: Add property for preloading camera interfaces at boot
    The number of camera interfaces that are going to be preloaded
    at service bootup can be set with 'persist.qmmf.preload.cameras'.
    
    Change-Id: I1ae069db53f8e3308e578699d4dd5a26b3784717

--- recorder/src/service/qmmf_recorder_service.cc
+++ recorder/src/service/qmmf_recorder_service.cc
@@ -41,6 +41,25 @@ RecorderService::RecorderService()
 
   QMMF_GET_LOG_LEVEL();
   QMMF_KPI_GET_MASK();
+
+  // Preload the recorder at bootup.
+  recorder_.reset(RecorderImpl::CreateRecorder());
+  if (!recorder_) {
+    QMMF_ERROR("%s: Can't create Recorder Instance!!", __func__);
+  } else {
+    std::function< const sp<RemoteCallBack>& (uint32_t id)>
+      remote_cb_handle = [&] (uint32_t id) {
+        QMMF_VERBOSE("%s: Client(%u): RemoteCallback request!", __func__, id);
+        assert(remote_cb_list_.count(id) != 0);
+        return remote_cb_list_[id];
+    };
+    auto ret = recorder_->Init(remote_cb_handle);
+    if (ret != NO_ERROR) {
+      QMMF_ERROR("%s: Recorder Initialization failed!", __func__);
+      recorder_.reset();
+    }
+  }
+
   QMMF_INFO("%s: RecorderService Instantiated! ", __func__);
   QMMF_KPI_DETAIL();
 }
@@ -501,13 +520,13 @@ status_t RecorderService::Connect(const sp<IRecorderServiceCallback>&
     }
     std::function< const sp<RemoteCallBack>& (uint32_t id)>
       remote_cb_handle = [&] (uint32_t id) -> sp<RemoteCallBack>& {
-        QMMF_VERBOSE("%s: Remote Callback request for client(%d)", __func__, id);
+        QMMF_VERBOSE("%s: Client(%u): RemoteCallback request!", __func__, id);
         assert(remote_cb_list_.count(id) != 0);
         return remote_cb_list_[id];
     };
     auto ret = recorder_->Init(remote_cb_handle);
     if (ret != NO_ERROR) {
-      QMMF_ERROR("%s: Recorder Initialization failed!", __func__);
+      QMMF_ERROR("%s: Recorder initialization failed!", __func__);
       recorder_.reset();
       return ret;
     }
