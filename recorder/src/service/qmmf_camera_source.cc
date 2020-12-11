f8b76fcc160d3e734f0f1b074a911eb969a1b224
Nikolay Hristozov <nhrist@codeaurora.org>
2020-12-11 15:44:03 +0200

    recorder: add lock before returning the buffer
    Fix for process_capture_result is stuck in qmmf server due to race
    condition for buffer_map_
    
    Change-Id: I4bcd35baf0799ba5ab3a47edab38f39eaa3e94a2

--- recorder/src/service/qmmf_camera_source.cc
+++ recorder/src/service/qmmf_camera_source.cc
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
+* Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -1632,6 +1632,7 @@ void TrackSource::OnFrameAvailable(StreamBuffer& buffer) {
   }
 
   if(IsPaused()) {
+    std::lock_guard<std::mutex> lock(frame_lock_);
     ReturnBufferToProducer(buffer);
     return;
   }
