6e334c3c838940c4e1661b883af14109207556a9
Aashish Patel <quic_paashish@quicinc.com>
2021-12-21 13:58:09 +0530

    cameraadaptor: fix marking related issue
    Issue: As part of 3870785, sequence of registering and submitting
    request was changed. We first submit and then register request if
    submit is successful. This is causing issue for scenario where
    result is returned before the request is registered.
    
    Fix: We revert back to previous sequence where the request is marked
    first and then submitted. The error scenario for submit failure is
    handled in error notify where the respective request is removed to
    avoid crash in result handling for unplug scenario.
    
    Change-Id: Id925b7e4967d6707dc7bcb4bc0367e35cc61f56c

--- common/cameraadaptor/qmmf_camera3_request_handler.cc
+++ common/cameraadaptor/qmmf_camera3_request_handler.cc
@@ -412,21 +412,21 @@ int32_t Camera3RequestHandler::SubmitRequest(CaptureRequest &nextRequest,
     totalNumBuffers++;
   }
 
-  // send capture request
-  res = hal3_device_->ops->process_capture_request(hal3_device_, &request);
-  if (0 != res) {
-    SIG_ERROR("%s: Unable to submit request %d in CameraHal : %s (%d)",
-              __func__, request.frame_number, strerror(-res), res);
-    HandleErrorRequest(request, nextRequest, outputBuffers);
-    return res;
-  }
-
   // Register capture request
   res = mark_cb_(request.frame_number, totalNumBuffers,
                  nextRequest.resultExtras);
   if (0 > res) {
     SIG_ERROR("%s: Unable to register new request: %s (%d)", __func__,
               strerror(-res), res);
+    HandleErrorRequest(request, nextRequest, outputBuffers);
+    return res;
+  }
+
+  // Send capture request
+  res = hal3_device_->ops->process_capture_request(hal3_device_, &request);
+  if (0 != res) {
+    SIG_ERROR("%s: Unable to submit request %d in CameraHal : %s (%d)",
+              __func__, request.frame_number, strerror(-res), res);
     HandleErrorRequest(request, nextRequest, outputBuffers);
     return res;
   }
