992d358c0d8122abc03435cc8da8c77f7ba62d34
Zhi Chen <quic_zhichen@quicinc.com>
2023-01-04 17:08:25 +0800

    qmmf-sdk: add configurable GBM device name
    Change-Id: I6e29ae8d7887408b937017c2ee0c2d3cc02915a9

--- common/memory/qmmf_gbm_interface.cc
+++ common/memory/qmmf_gbm_interface.cc
@@ -362,7 +362,7 @@ GBMDevice::GBMDevice() {
   gbm_fd_ = open("/dev/dri/renderD128", O_RDWR);
   if (gbm_fd_ < 0) {
     QMMF_WARN("%s: Falling back to /dev/ion \n", __func__);
-    gbm_fd_ = open("/dev/ion", O_RDWR);
+    gbm_fd_ = open(GBM_DEV_NAME, O_RDWR);
   }
   assert(gbm_fd_ >= 0);
   gbm_device_ = gbm_create_device(gbm_fd_);
