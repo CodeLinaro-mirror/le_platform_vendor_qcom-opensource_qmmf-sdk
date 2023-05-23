a6e4653c46d4f8b3377c17efc63fbcbfe62e53d1
Kin Zhao <quic_yunjiez@quicinc.com>
2023-05-23 15:39:46 +0800

    qmmf-sdk: add image NV12 enable flag
    Change-Id: I04bd6cf58c2a93219317b564722d0770f04d9dbd

--- recorder/src/service/qmmf_camera_context.cc
+++ recorder/src/service/qmmf_camera_context.cc
@@ -592,11 +592,13 @@ status_t CameraContext::ConfigImageCapture(const SnapshotParam& param,
       ret = GetSnapshotStreamParams(param, stream_param);
       assert(ret == NO_ERROR);
 
+#ifdef ENABLE_IMAGE_NV12
       if (param.format == BufferFormat::kNV12) {
         stream_param.allocFlags.flags |= IMemAllocUsage::kHwCameraWrite;
         stream_param.data_space = static_cast<android_dataspace_t>
                                   (HAL_DATASPACE_HEIF);
       }
+#endif
 
       ret = CreateSnapshotStream(stream_param, true);
       if (NO_ERROR != ret) {
