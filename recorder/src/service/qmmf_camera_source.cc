70e46e77dd983a8ec1df64d773189a0b2174841f
Shyam Komakula <quic_skomakul@quicinc.com>
2023-05-25 12:18:03 +0530

    qmmf-sdk: Add support for Stream Orientation
    configure rotation based on the rotate property value
    
    Change-Id: I295d84d777ffe50ae751f89e33ffd1b3ae6ac163

--- recorder/src/service/qmmf_camera_source.cc
+++ recorder/src/service/qmmf_camera_source.cc
@@ -28,7 +28,7 @@
 *
 * Changes from Qualcomm Innovation Center are provided under the following license:
 *
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted (subject to the limitations in the
@@ -270,6 +270,7 @@ status_t CameraSource::ConfigImageCapture(const uint32_t camera_id,
   sparam.height  = param.height;
   sparam.format  = Common::FromImageToQmmfFormat(param.format);
   sparam.quality = param.quality;
+  sparam.rotation = param.rotation;
 
   auto ret = camera->ConfigImageCapture(sparam, xtraparam);
   if (ret != NO_ERROR) {
