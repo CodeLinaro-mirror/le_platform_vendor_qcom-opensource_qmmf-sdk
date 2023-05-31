4f435cf6dbd475039353846f21127dc6d143a3d9
Amrutha Sai Gattu <quic_agattu@quicinc.com>
2023-05-30 18:58:55 -0700

    qmmf-sdk: recorder client: fix handling of GBM buffer FD
    Previous version of GBM does not provide FD ownership to
    client so we don't have to close the fd there. In new
    version, the gbm client has to close it explicitly
    
    Change-Id: Ie23863cb271359d6c25fa766cc22127b1de05c17

--- recorder/src/client/qmmf_recorder_client.cc
+++ recorder/src/client/qmmf_recorder_client.cc
@@ -1092,11 +1092,13 @@ status_t RecorderClient::UnmapBuffer(BufferInfo& info) {
 #ifdef TARGET_USES_GBM
     ReleaseBuffer(info.ion_fd);
 #endif
+#ifdef GBM_FREE_FD
     if ((info.ion_fd != -1) && (close(info.ion_fd) < 0)) {
       QMMF_ERROR("%s() error closing shared fd[%d]: %d[%s]", __func__,
                  info.ion_fd, errno, strerror(errno));
       return errno;
     }
+#endif
     info.ion_fd = -1;
   }
 
