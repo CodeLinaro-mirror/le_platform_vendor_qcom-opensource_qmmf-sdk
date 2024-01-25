35cc6a770a9b35aa2c8e9305c656fa5dd7c29ce6
Girish K <quic_girik@quicinc.com>
2024-01-25 16:27:34 +0530

    qmmf-sdk: AOSP hal-adaptation moved to qmmf
    Moved Camera metadata and vendor-tags from AOSP adaptation
    
    Change-Id: I5673abe9a1fae5dd543ac3fda6ffae3e16d1fdec

--- recorder/src/service/qmmf_remote_cb.cc
+++ recorder/src/service/qmmf_remote_cb.cc
@@ -26,9 +26,9 @@
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
-* Changes from Qualcomm Innovation Center are provided under the following license:
+* Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
 *
-* Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 *  
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted (subject to the limitations in the
@@ -137,7 +137,7 @@ void RemoteCallBack::NotifyVideoTrackEvent(uint32_t session_id,
 }
 
 void RemoteCallBack::NotifyCameraResult(uint32_t camera_id,
-                                        const ::camera::CameraMetadata &result) {
+                                        const CameraMetadata &result) {
   assert(client_cb_handle_.get() != nullptr);
   client_cb_handle_->NotifyCameraResult(camera_id, result);
 }
