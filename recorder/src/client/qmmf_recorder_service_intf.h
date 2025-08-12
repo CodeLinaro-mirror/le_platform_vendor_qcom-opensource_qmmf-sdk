4342cb016ea1231b82590a99b7fda80927aa9236
Guangde <guangde@qti.qualcomm.com>
2025-08-12 18:54:53 +0800

    qmmf-sdk: support two input for IPE offline
    Modify IPE offline from one input to two input by default.
    
    Change-Id: Ia2fc35d6226eadeb6626b54a452d8eb1293f3d6c

--- recorder/src/client/qmmf_recorder_service_intf.h
+++ recorder/src/client/qmmf_recorder_service_intf.h
@@ -289,7 +289,8 @@ class IRecorderService : public IInterface {
                                 const OfflineCameraCreateParams& params) = 0;
 
   virtual status_t ProcOfflineProcess(const uint32_t client_id,
-                                     const BnBuffer& in_buf,
+                                     const BnBuffer& in_buf0,
+                                     const BnBuffer& in_buf1,
                                      const BnBuffer& out_buf,
                                      const CameraMetadata& meta) = 0;
 
